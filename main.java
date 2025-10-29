8eebb5e4fa8b540debe72298137ba361b933d65d
harshkumarsingh872-png <harshkumarsingh872@gmail.com>
2025-10-29 11:46:43 +0530

    this is java add prog

---
+++ main.java
@@ -0,0 +1,15 @@
+ class add {
+    int a=5;
+    int b=7;
+    int c=a+b;
+}
+
+public class main{
+    public static void main(String[] args) {
+        
+    
+    add s1 = new add();
+    System.out.println("addition of a and b is "+s1.c);
+}
+
+}
